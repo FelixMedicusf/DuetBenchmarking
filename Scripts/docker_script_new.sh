84fb63b62d47c75ed281fc37d83ec810fa72ccbe
FelixMedicusf <felixbieleit@gmail.com>
2022-05-26 14:25:10 +0200

    .

--- Scripts/docker_script_new.sh
+++ Scripts/docker_script_new.sh
@@ -17,15 +17,15 @@ echo $instanceName
 
 # Copy data.cql from host to home directory of VM 
 gcloud compute scp --zone europe-west1-b ~/Documents/data.cql $instanceName:~
-
+# Create Docker Network a and b 
+gcloud compute ssh $instanceName --zone europe-west1-b -- 'sudo docker network create cassandra-network-a'
+# gcloud compute ssh $instanceName --zone europe-west1-b -- 'sudo docker network create cassandra-network-b'
 
 if [[ $i -eq 1 ]];then
 
 seedIp=$nodeIp
 
-# Create Docker Network a and b 
-gcloud compute ssh $instanceName --zone europe-west1-b -- 'sudo docker network create cassandra-network-a'
-gcloud compute ssh $instanceName --zone europe-west1-b -- 'sudo docker network create cassandra-network-b'
+
  
 # Cassandra uses port 7000 for Internode Communication, 7001 for TLS internode communication, 9160 as Thrift Client API, and 9042 as CQL native transport port 
 # -p 8080:80 --> Map TCP port 80 in the container to port 8080 on the Docker host. -p <host_port>:<container_port>
@@ -63,6 +63,7 @@ cmd="sudo docker run --name cassandra-container-1a -d --rm\
                         -e CASSANDRA_BROADCAST_ADDRESS=$nodeIp\
                         -e CASSANDRA_SEEDS=$seedIp\
                         --hostname cassandra-container-1a\
+                        --network cassandra-network-a\
                         -v /docker/cassandra/container-1a:/var/lib/cassandra\
                         -p 9042:9042\
                         -p 7000:7000\
@@ -85,7 +86,7 @@ echo "Started second Container (1a) on port 7000 and 9042"
 fi 
 # echo "Started second Container (1b) on port 7001 and 9043"
 echo "Waiting for Container 1a and 1b to run to load data"
-sleep 30 
+sleep 35 
 
 # Load CQL file into cassandra-container-1b and cassandra-container-1a
 # CAUTION: CQLVERSION must fit to respective Cassandra Version

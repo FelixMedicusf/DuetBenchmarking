69e8733378feb671f5e8fa909db80fd44be82ef6
FelixMedicusf <felixbieleit@gmail.com>
2022-06-10 14:10:59 +0200

    \"

--- Scripts/docker_script_new.sh
+++ Scripts/docker_script_new.sh
@@ -33,8 +33,8 @@ firstInstanceName=$currentInstanceName
 # Cassandra uses port 7000 for Internode Communication, 7001 for TLS internode communication, 9160 as Thrift Client API, and 9042 as CQL native transport port 
 # -p 8080:80 --> Map TCP port 80 in the container to port 8080 on the Docker host. -p <host_port>:<container_port>
 
-command1 = "cd ~ && printf "cluster_name: 'Cassandra Cluster A' \nstorage_port: 7005" > ~/cassandraA.yaml"
-command2 = "cd ~ && printf "cluster_name: 'Cassandra Cluster B' \nstorage_port: 7010" > ~/cassandraB.yaml"
+command1 = "cd ~ && printf \"cluster_name: 'Cassandra Cluster A' \nstorage_port: 7005\" > ~/cassandraA.yaml"
+command2 = "cd ~ && printf \"cluster_name: 'Cassandra Cluster B' \nstorage_port: 7010\" > ~/cassandraB.yaml"
 gcloud compute ssh $currentInstanceName --zone europe-west1-b -- $command1  
 gcloud compute ssh $currentInstanceName --zone europe-west1-b -- $command2
 
@@ -75,8 +75,8 @@ if [[ $i -ne 1 ]]; then
 # TO DO: Write script to add the seed_ip addresses to the cassandra1.yaml and cassandra2.yaml and then pass as argument to docker run (mount in container)
 
 if [[$i -eq 2]];then
-command1 = "cd ~ && printf "\nseed_provider:\n  - class_name: org.apache.cassandra.locator.SimpleSeedProvider\n    - seeds: $seedIp" >> ~/cassandraA.yaml"
-command2 = "cd ~ && printf "\nseed_provider:\n  - class_name: org.apache.cassandra.locator.SimpleSeedProvider\n    - seeds: $seedIp" >> ~/cassandraB.yaml"
+command1 = "cd ~ && printf \"\nseed_provider:\n  - class_name: org.apache.cassandra.locator.SimpleSeedProvider\n    - seeds: $seedIp\" >> ~/cassandraA.yaml"
+command2 = "cd ~ && printf \"\nseed_provider:\n  - class_name: org.apache.cassandra.locator.SimpleSeedProvider\n    - seeds: $seedIp\" >> ~/cassandraB.yaml"
 gcloud compute ssh $currentInstanceName --zone europe-west1-b -- $command1
 gcloud compute ssh $currentInstanceName --zone europe-west1-b -- $command2
 
